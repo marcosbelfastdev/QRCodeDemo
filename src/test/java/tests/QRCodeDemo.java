6d28a51b54e25dab8d92804adca3991dfa4bfe01
marcosbelfastdev <marcosbelfastdev@gmail.com>
2021-05-02 00:39:26 -0300

    No message

--- src/test/java/tests/QRCodeDemo.java
+++ src/test/java/tests/QRCodeDemo.java
@@ -7,6 +7,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -24,12 +26,12 @@ public class QRCodeDemo {
 
     @Before
     public void setup() {
+
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("disable-notifications");
         options.addArguments("--incognito");
         options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
-        options.setExperimentalOption("useAutomationExtension", false);
         webDriver = new ChromeDriver(options);
 
         BaseState baseState = new BaseState();
@@ -38,12 +40,13 @@ public class QRCodeDemo {
         baseState.setTestName("Demo");
         baseState.setBaseStateUrl("https://www.patrick-wied.at/static/qrgen/");
         browser = baseState.execute(webDriver);
-        browser.manage().window().maximize();
+        browser.manage().window().setPosition(new Point(600,1));
+        browser.manage().window().setSize(new Dimension(600,2000));
         browser.manage().deleteAllCookies();
 
         browser.setOption(Common.SCROLL_TO_ELEMENTS);
         browser.setOption(Common.HIGHLIGHT_ELEMENTS);
-        browser.setOption(Common.HIGHLIGHT_STYLE, "border: 3px solid purple; border-radius: 5px;");
+        browser.setOption(Common.HIGHLIGHT_STYLE, "border: 4px solid purple; border-radius: 5px;");
     }
 
     @Test
@@ -58,17 +61,20 @@ public class QRCodeDemo {
         while (attempts++ < 5) {
 
             String randomNumber = String.valueOf(random.nextLong());
+
             browser.find(By.id("content")).setText(randomNumber);
             browser.find(By.id("button")).click();
 
-            // espera qrcode ser atualizado e destaca com highlight
             EElement image = browser.find(By.id("qr-image"));
             image.highlight();
-            while (!image.getAttribute("alt").trim().contains(randomNumber)) {}
+            while (!image.getAttribute("alt").trim().contains(randomNumber)) {
 
-            // lê imagem como BufferedImage
-            QRCodeReader qrCodeReader = new QRCodeReader();
-            String returnedQrCodeString = qrCodeReader.readQRCode(getBufferedImage(image));
+            }
+
+            String returnedQrCodeString = new QRCodeReader()
+                    .readQRCode(
+                            getBufferedImage(image)
+                    );
 
             System.out.println("Informado: " + randomNumber + "\t Obtido: " + returnedQrCodeString);
             Assert.assertEquals(randomNumber, returnedQrCodeString);
